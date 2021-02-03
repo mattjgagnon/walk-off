7959727ab87e303335b9f794327d04b5a78a4461
Matt Gagnon <mgagnon@sendlane.com>
2021-02-02 20:07:54 -0800

    Add more Game logic and unit tests

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,16 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
+    use HasFactory;
+
+    private bool $start;
     private array $teams;
 
     public function __construct()
     {
         parent::__construct();
         $this->teams = [];
+    }
+
+    public function start(): bool
+    {
+        if (count($this->teams) > 1) {
+            $this->start = true;
+        } else {
+            throw new \Exception('Not enough teams to start a game');
+        }
+
+        return $this->start;
     }
 
     public function getTeams(): array
--- tests/Unit/GameTest.php
+++ tests/Unit/GameTest.php
@@ -25,4 +25,32 @@ class GameTest extends TestCase
         $team3 = Team::factory()->make();
         $game->setTeam($team3);
     }
+
+    /**
+     * @test
+     */
+    public function can_start_a_new_game_with_two_teams()
+    {
+        $team1 = Team::factory()->make();
+        $team2 = Team::factory()->make();
+        $game = new Game();
+        $game->setTeam($team1);
+        $game->setTeam($team2);
+        $game->start();
+
+        $this->assertTrue($game->start());
+    }
+
+    /**
+     * @test
+     */
+    public function trying_to_start_a_new_game_with_less_than_two_teams_throws_an_exception()
+    {
+        $team1 = Team::factory()->make();
+        $game = new Game();
+        $game->setTeam($team1);
+
+        $this->expectException('Exception');
+        $this->assertTrue($game->start());
+    }
 }
